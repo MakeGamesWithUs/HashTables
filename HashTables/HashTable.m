dc08bb7d1f705877820feea425c2dcf0be5119bd
Daniel Haaser <thedapperdan@gmail.com>
2014-11-26 14:45:09 -0800

    Merge comment change

--- HashTables/HashTable.m
+++ HashTables/HashTable.m
@@ -33,6 +33,9 @@ static const NSInteger kBucketCount = 128;
 #pragma mark - Utilities
 #pragma message "Implement hashForString: !"
 
+// Convert the key string into an unsigned integer between the range 0 and MAX_INT
+// Ideally it's an evenly distributed number from 0 to MAX_INT
+
 - (NSUInteger)hashForString:(NSString*)string
 {
     return 0;
